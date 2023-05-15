765d220883763e99f6e537c06869845e535f34d7
icedterminal <icedterminal@gmail.com>
2023-05-15 09:57:07 -0700

    Different wow sync scripts for different choices.

--- WowSync.ps1
+++ WowSync.ps1
@@ -3,8 +3,8 @@
 WoW Sync
 
 .DESCRIPTION
-v1.0.1
-Sync your World of Warcraft Toon Folder (WTF) with git accounts.
+v1.0.0
+Sync your World of Warcraft Interface AddOns, WTF, and Fonts with git accounts.
 
 .LINK
 https://github.com/icedterminal/PS-Scripts
@@ -29,16 +29,15 @@ Right click this file and click "Run with PowerShell"
 # First move to the root of WoW. You may need to alter this path if you installed WoW somewhere else.
 Set-Location "C:\Program Files\World of Warcraft"
 
-# Then look through for different install versions with WTF.
-Get-ChildItem -Path "_*_" -Recurse -Filter "WTF" | ForEach-Object {
+# Then look through for different install versions.
+Get-ChildItem -Path "_*_" | ForEach-Object {
 	# Once they are found, check the git status for untracked changes and files.
 	if (Set-Location $_ && git status | Select-String -Pattern "Changes not staged|Untracked files" ) {
 		# Before pushing, a branch has to be specified. There is already a variable that collects this information: the path.
 		# The path must be converted to a string.
 		$path = Convert-Path $_
 		# Trim the path string down to the version using regex and use that as the branch to push to.
-		# If you want to see the regex in action: https://regexr.com/7dlfr
-		$branch = $path -replace '^[^_]*_|_\\(?:.(?!\\))+$',''
+		$branch = $path -replace '^[^_]*_|_+$',''
 		write-host "`nChecking $branch"
 		write-host "Syncing..." -ForegroundColor Yellow
 		# Give the commit message the date and time. Easy to sort through.
@@ -56,7 +55,7 @@ Get-ChildItem -Path "_*_" -Recurse -Filter "WTF" | ForEach-Object {
 		# Again converting path to string and trimming.
 		# This time we just report no changes and close.
 		$path = Convert-Path $_
-		$branch = $path -replace '^[^_]*_|_\\(?:.(?!\\))+$',''
+		$branch = $path -replace '^[^_]*_|_+$',''
 		write-host "`nChecking $branch"
 		write-host "No changes" -ForegroundColor Green
 		# Pause for a few seconds to review.
